28592b9aed2e85d4c86b0304ef102dd2f3bb259d
DixonBriones <dixonbriones@hotmail.com>
2022-07-04 01:15:36 -0500

    UsuarioAuto arreglo

--- src/controllers/auto.controller.js
+++ src/controllers/auto.controller.js
@@ -17,7 +17,12 @@ const readAuto = async (req, res) => {
 
 const readAutoByID = async (req, res) => {
   const{id}=req.params;
-  const data = await AutoModel.findById(id)
+  const datos = await AutoModel.findById(id).populate({ path: 'usuario', select: 'username -_id' })
+  const {username, ...rest }=datos.usuario
+  const data = datos.toObject();
+  delete data["usuario"];
+  delete data["__v"]
+  data.usuario=username
   res.status(200).json(data)
 }
 
