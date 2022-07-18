2e25379c8ba201c57d2a8c7b79161baf4558827f
Michael J Mero A <maicol23mero@gmail.com>
2022-07-17 21:50:29 -0500

    Listar autos por usuario

--- src/controllers/auto.controller.js
+++ src/controllers/auto.controller.js
@@ -1,6 +1,8 @@
 const {response}= require('express')
 const { AutoModel } = require('../models')
 
+const jwt = require('jsonwebtoken')
+
 
 const insertarAuto = async (req, res = response) => {
     const{...body}=req.body;
@@ -8,7 +10,7 @@ const insertarAuto = async (req, res = response) => {
     const autos = new AutoModel(body);
     const nuevoAuto= await autos.save();
     res.json(nuevoAuto);
-  }
+}
 
 const readAuto = async (req, res) => {
     const data = await AutoModel.find()
@@ -26,10 +28,16 @@ const readAutoByID = async (req, res) => {
   res.status(200).json(data)
 }
 
+const readAutoUser = async (req, res) =>{
+  const data = await AutoModel.find({usuario: req.userId})
+  console.log(data)
+  res.status(200).json(data)
+}
+
 const deleteAuto = async (req, res) =>{
   const { id } = req.params;
   const autoDelete = await AutoModel.findByIdAndRemove(id)
   res.status(200).json({msg: `El auto fue eliminado satisfactoriamente`})
 }
 
-module.exports={insertarAuto,readAuto,readAutoByID, deleteAuto};
+module.exports={insertarAuto,readAuto,readAutoByID, deleteAuto, readAutoUser};
--- src/routes/auto.routes.js
+++ src/routes/auto.routes.js
@@ -2,10 +2,13 @@ const express = require('express')
 const {controllers} = require('../controllers')
 const AutoRouter = express.Router()
 
+const { verifyToken } = require('../middlewares')
+
 AutoRouter.post('/', controllers.ApiAutoController.insertarAuto)
 AutoRouter.get('/', controllers.ApiAutoController.readAuto)
-AutoRouter.get('/findById/:id',controllers.ApiAutoController.readAutoByID)
+//AutoRouter.get('/findById/:id',controllers.ApiAutoController.readAutoByID)
 
-AutoRouter.delete('/:id',controllers.ApiAutoController.deleteAuto)
+AutoRouter.get('/:id', verifyToken.verifyToken, controllers.ApiAutoController.readAutoUser)
+AutoRouter.delete('/:id', controllers.ApiAutoController.deleteAuto)
 
 module.exports = AutoRouter
