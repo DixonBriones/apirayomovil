4575b23d5736e7a5393ad1a7e856d3bc05592c0e
DixonBriones <dixonbriones@hotmail.com>
2022-07-26 01:29:01 -0500

    alquiler

--- src/controllers/alquiler.controller.js
+++ src/controllers/alquiler.controller.js
@@ -1,13 +1,34 @@
 const {response}= require('express')
-const { AlquilerModel } = require('../models')
+const { AlquilerModel,AutoModel } = require('../models')
 
 const alquiler = async (req, res) =>{
     const { ...body } = req.body
+    await AutoModel.findByIdAndUpdate(body.auto,{status:false})
     const data = new AlquilerModel(body)
     const datos = await data.save()
-    res.json(datos)
+    res.json(data)
+}
+
+const busquedaalquilerID = async (req, res) =>{
+    const id= req.userId
+    const datos = await AlquilerModel.find({usuario: id}).populate({path: 'auto',populate : { path : 'usuario'}})
+    datos.forEach((dato,index)=>{
+        const data = dato.toObject();
+        const {telefono, ...rest }=dato.auto.usuario
+        const {marca,modelo,foto, ...rest2 }=dato.auto
+        delete data["usuario"];
+        delete data["auto"];
+        delete data["__v"]
+        data.telefono=telefono
+        data.marca=marca
+        data.modelo=modelo
+        data.foto=foto
+        datos[index]=data
+    })
+    res.status(200).json(datos)
 }
 
 module.exports = {
-    alquiler
+    alquiler,
+    busquedaalquilerID
 }
--- src/routes/alquiler.routes.js
+++ src/routes/alquiler.routes.js
@@ -1,7 +1,8 @@
 const ruta = require('express').Router()
 const { controllers } = require('../controllers')
-
+const { verifyToken } = require('../middlewares')
 
 ruta.post('/insertaralquiler', controllers.ApiAlquilerController.alquiler)
+ruta.get('/findalquilerid',verifyToken.verifyToken, controllers.ApiAlquilerController.busquedaalquilerID)
 
 module.exports = ruta
